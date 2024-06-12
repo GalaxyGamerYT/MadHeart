43debabfe07b6ea02cf8694b460e969d9c6fe7de
GalaxyGamerYT <OliWootton@Outlook.com>
2024-06-12 13:00:37 +0100

    remaking settings

--- autoloads/global_settings.gd
+++ autoloads/global_settings.gd
@@ -2,17 +2,17 @@ extends SettingsCollection
 
 # Settings
 var v_sync_setting = BoolSetting.new("V-Sync", "graphics", "Controls whether the game uses V-Sync.")
-var window_mode_setting = MultiChoiceSetting.new("Window Mode", "window", "Controls the window mode.", "Windowed", ["Fullscreen", "Borderless Fullscren", "Windowed", "Borderless Windowed"])
+var window_mode_setting = MultiChoiceSetting.new("Window Mode", "window", "Controls the window mode.", "Windowed", ["Windowed", "Borderless Windowed", "Fullscreen", "Borderless Fullscreen"])
 
 func _init():
 	add_setting(v_sync_setting)
 	add_setting(window_mode_setting)
 	
 	var keys:Dictionary = get_keys()
-	
+
 	# Input Settings
 	for keyName in keys:
-		print(keys[keyName]["key"])
+		#print(keys[keyName]["key"])
 		var key_setting = KeySetting.new(keyName+" Key", "Input", "The key used to "+keys[keyName]["tooltip"], keys[keyName]["key"])
 		add_setting(key_setting)
 
@@ -65,30 +65,42 @@ func on_v_sync_updated():
 		ProjectSettings["display/window/vsync/vsync_mode"] = DisplayServer.VSYNC_DISABLED
 
 func on_window_mode_updated():
+	print("HERE")
 	var mode:String = window_mode_setting.get_value()
 	var window := get_window()
-	if mode == "Fullscreen":
-		window.borderless = false
-		window.mode = Window.MODE_FULLSCREEN
-		Input.mouse_mode = Input.MOUSE_MODE_CONFINED
-		window.always_on_top = true
-	elif mode == "Borderless Fullscreen":
-		window.borderless = true
-		window.mode = Window.MODE_FULLSCREEN
-		Input.mouse_mode = Input.MOUSE_MODE_CONFINED
-		window.always_on_top = true
-	elif mode == "Windowed":
-		window.borderless = false
-		window.mode = Window.MODE_WINDOWED #Exclusive has no effect.
-		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-		window.always_on_top = false
-	elif mode == "Borderless Windowed":
-		window.borderless = true
-		window.mode = Window.MODE_WINDOWED
-		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-		window.always_on_top = false
-
-
+	match mode:
+		"Fullscreen":
+			#window.borderless = false
+			#window.mode = Window.MODE_FULLSCREEN
+			#Input.mouse_mode = Input.MOUSE_MODE_CONFINED
+			#window.always_on_top = true
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+			DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, true)
+			print("Fullscreen")
+		"Borderless Fullscreen":
+			#window.borderless = true
+			#window.mode = Window.MODE_FULLSCREEN
+			#Input.mouse_mode = Input.MOUSE_MODE_CONFINED
+			#window.always_on_top = true
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+			DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, true)
+			print("Borderless Fullscreen")
+		"Windowed":
+			#window.borderless = false
+			#window.mode = Window.MODE_WINDOWED #Exclusive has no effect.
+			#Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+			#window.always_on_top = false
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+			DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, false)
+			print("Windowed")
+		"Borderless Windowed":
+			#window.borderless = true
+			#window.mode = Window.MODE_WINDOWED
+			#Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+			#window.always_on_top = false
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+			DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, true)
+			print("Borderless Windowed")
 
 func _exit_tree():
 	save_to_GSON(GameController.global_settings_GSON)
